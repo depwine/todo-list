97e771e46d3a7b719707de50928316b1984ea191
Glib Lakeev <glakeev@gmail.com>
2022-09-22 13:09:10 -0400

    adding 2 more files, forgot to save

--- src/index.js
+++ src/index.js
@@ -12,9 +12,7 @@ taskList();
 
 window.addEventListener(`load`, () => {
 
-    const form = document.querySelector("#new-task-form");
-    const input = document.querySelector("#new-task-input");
-    const list_el = document.querySelector("#tasks")
+
 
     task();
 
